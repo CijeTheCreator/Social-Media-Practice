2d08653800c44d4d20ddd1e7edb309d4c07f0f12
Osadebe Chijioke Akaolisa <osadebec98@gmail.com>
2022-03-15 12:04:30 +0100

    Added the conditional routing in app.js based on the availablity of User:context

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
   console.log(process.env.REACT_APP_PUBLIC_FOLDER);
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes path="/">
-        <Route exact path="/" element={<Home></Home>}></Route>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home></Home> : <Login />}
+        ></Route>
         <Route exact path="/profile/:id" element={<Profile />}></Route>
-        <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/register" element={<Register />}></Route>
+        <Route
+          exact
+          path="/login"
+          element={!user ? <Login /> : <Home></Home>}
+        ></Route>
+        <Route
+          exact
+          path="/register"
+          element={!user ? <Register /> : <Home></Home>}
+        ></Route>
       </Routes>
     </Router>
   );
