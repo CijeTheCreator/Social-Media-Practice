839956c499d40a7f04702ac4d462ed434ea480ab
Osadebe Chijioke Akaolisa <osadebec98@gmail.com>
2022-03-15 12:23:04 +0100

    Implemented the Register functionality

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,8 +1,30 @@
+import { useRef } from "react";
 import "./register.css";
 
 function Register() {
+  const email = useRef();
+  const password = useRef();
+  const confirmPassword = useRef();
+  const firstName = useRef();
+  const lastName = useRef();
+  const history = useHistory(); //import from react-router-dom
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (password.current.value !== confirmPassword.current.value) {
+      confirmPassword.current.setCustomValidity("Password's do not match");
+    } else {
+      const usableFirstName = firstName.current.value;
+      const usableLastName = lastName.current.value;
+      const usableConfirmPassword = confirmPassword.current.value;
+      const usablePassword = password.current.value;
+      const usableEmail = email.current.value;
+      const response = axios("post", {
+        name: usableFirstName,
+        email: usableEmail,
+        password: usablePassword,
+      }); //Todo I did not put the api side on github, so complete is based on this
+      history.push("/");
+    }
   };
   return (
     <div className="loginWrapper">
@@ -15,24 +37,44 @@ function Register() {
         </div>
         <div className="form">
           <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="First Name" required name="name" />
-            <input type="text" placeholder="Last Name" required name="name" />
-            <input type="email" placeholder="Email" required name="email" />
+            <input
+              type="text"
+              placeholder="First Name"
+              required
+              name="first-name"
+              ref={firstName}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              required
+              name="last-name"
+              ref={lastName}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              required
+              name="email"
+              ref={email}
+            />
             <input
               type="password"
               placeholder="Password"
               required
-              name="email"
+              name="password"
               minLength={6}
+              ref={password}
             />
             <input
               type="password"
               placeholder="Confirm Password"
               required
-              name="email"
+              name="confirm-password"
               minLength={6}
+              ref={confirmPassword}
             />
-            <button>Sign Up</button>
+            <button type="submit">Sign Up</button>
           </form>
           <p className="forgot">Already an account?</p>
           <button className="createNew">Sign In</button>
